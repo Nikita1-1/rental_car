3ba79ae2a3628f32d742a1a4081be19b7e753403
nikita <sazonovn2018@gmail.com>
2025-07-08 13:17:32 -0600

    changed settings

--- rental_car_app/rental_car/settings.py
+++ rental_car_app/rental_car/settings.py
@@ -30,13 +30,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure--qd_mo+(c$^pd%!3z((t-t0@96qg*cryrsxgom3t7g)xhdp72+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool) 
 
 # ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost').split(',')
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost').split(',')
 
 # Application definition
 
